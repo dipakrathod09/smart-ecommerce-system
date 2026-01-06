4ae02785ef192fe2552d72cb04c67496d6724dde
dipakrathod09 <dipakrathod0909@gmail.com>
2026-01-06 22:50:08 +0530

    Session-Based Login & Logout

--- app.py
+++ app.py
@@ -1,12 +1,21 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, redirect, url_for, session
 from werkzeug.security import generate_password_hash, check_password_hash
 from db import get_connection
 
 app = Flask(__name__)
+app.secret_key = "supersecretkey"
 
 @app.route('/')
 def home():
     return render_template('index.html')
+
+@app.route('/dashboard')
+def dashboard():
+    if 'user_id' not in session:
+        return redirect(url_for('login'))
+
+    return render_template('dashboard.html', name=session['user_name'])
+
 
 @app.route('/register', methods=['GET', 'POST'])
 def register():
@@ -48,11 +57,19 @@ def login():
         conn.close()
 
         if user and check_password_hash(user[3], password):
-            return f"Welcome {user[1]}"
+            session['user_id'] = user[0]
+            session['user_name'] = user[1]
+            session['role'] = user[4]
+            return redirect(url_for('dashboard'))
         else:
-            return "Invalid Email or Password"
+            return "Invalid email or password"
 
     return render_template('login.html')
+
+@app.route('/logout')
+def logout():
+    session.clear()
+    return redirect(url_for('login'))
 
 if __name__ == '__main__':
     app.run(debug=True)
