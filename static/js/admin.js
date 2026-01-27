b81eb00fdaa0b7216c36df3eb265a3ef3bbef4d3
dipakrathod09 <dipakrathod0909@gmail.com>
2026-01-27 21:19:52 +0530

    Added cart, admin navbar, products and register pages

--- static/js/admin.js
+++
@@ -1,6 +0,0 @@
-function confirmDelete(url) {
-    if (confirm("Are you sure you want to delete this product?")) {
-      window.location.href = url;
-    }
-  }
-  
--- static/js/cart.js
+++
@@ -1,5 +0,0 @@
-function updateQty(productId, change) {
-    fetch(`/update-cart/${productId}/${change}`)
-      .then(() => location.reload());
-  }
-  
